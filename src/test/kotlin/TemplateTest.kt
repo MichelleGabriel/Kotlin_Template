e1e69350db320235691489b4e1120064113759e5
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-22 15:31:08 +0200

    VowelCount Kata

--- src/test/kotlin/TemplateTest.kt
+++ src/test/kotlin/TemplateTest.kt
@@ -1,9 +1,11 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class TemplateTest {
+class TestExample {
     @Test
-    fun `pass`() {
-        assertTrue(true)
+    fun testFixed() {
+        assertEquals(5, getCount("abracadabra"))
+        assertEquals(1, getCount("test"))
+        assertEquals(3, getCount("example"))
     }
 }
