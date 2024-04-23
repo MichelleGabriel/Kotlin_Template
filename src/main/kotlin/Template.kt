09224fdc70bf74ba7c98b4c5b5901a78be6d844c
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-23 10:08:54 +0200

    CountingDuplicates

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,14 @@
-class Template {
-    fun foo() = true
+fun duplicateCount(text: String): Int {
+    var duplicate = 0
+    val groupingBy = text.groupBy { it }
+//    Fuer jede List(Char) pruefe ob der Wert > 1. Wenn ja zieh Zaehler hoch.
+
+
+
+    groupingBy.forEach {
+        if (it.value.size > 1) {
+           duplicate++
+        }
+    }
+    return duplicate
 }
