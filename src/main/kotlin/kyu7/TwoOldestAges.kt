47791cc46b118b3b137852a14ded5c7f88e2d280
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-23 09:14:40 +0200

    TwoOldesAges Code Refactor

--- src/main/kotlin/kyu7/TwoOldestAges.kt
+++ src/main/kotlin/kyu7/TwoOldestAges.kt
@@ -2,8 +2,6 @@ package kyu7
 
 // https://www.codewars.com/kata/511f11d355fe575d2c000001
 
-fun twoOldestAges(ages: List<Int>): List<Int> {
-
-   return ages.sorted().takeLast(2)
-
-}
+fun twoOldestAges(ages: List<Int>) = ages
+   .sorted()
+   .takeLast(2)
