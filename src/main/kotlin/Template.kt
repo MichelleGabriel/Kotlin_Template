0a20b871a4ba4b7c75b30a4e4ae27cf416cb8bc8
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-25 09:46:05 +0200

    RGB to Hex Solution

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,29 @@
-class Template {
-    fun foo() = true
+@file:OptIn(ExperimentalStdlibApi::class)
+
+
+@OptIn(ExperimentalStdlibApi::class)
+fun rgb(r: Int, g: Int, b: Int): String {
+
+    var firstHex = r.coerceIn(0,255).toHexString(format = HexFormat.UpperCase).takeLast(2)
+    var secondHex = g.coerceIn(0,255).toHexString(format = HexFormat.UpperCase).takeLast(2)
+    var thirdHex = b.coerceIn(0,255).toHexString(format = HexFormat.UpperCase).takeLast(2)
+
+    println()
+
+    return toHex(r) + toHex(g) + toHex(b)
+    //return "${toHex(r)}${toHex(g)}${toHex(b)}"
 }
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun toHex(color: Int) = when {
+    color < 0 -> "00"
+    color > 255 -> "FF"
+    else -> color.toHexString(format = HexFormat.UpperCase).takeLast(2)
+}
+
+//Option mit .coerceIn direkt in der Methode
+@OptIn(ExperimentalStdlibApi::class)
+private fun Int.toHexColor() = this
+    .coerceIn(0,255)
+    .toHexString(format = HexFormat.UpperCase)
+    .takeLast(2)
