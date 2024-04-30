e3bd6bb51eb3a595e21427808eab9c9d568ca34d
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-30 09:15:55 +0200

    DeleteOccurenceOfAnElement Solution

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,15 @@
-class Template {
-    fun foo() = true
+object EnoughIsEnough {
+    fun deleteNth(elements: IntArray, maxOcurrences: Int): IntArray {
+
+        //Wenn elements, das element oefter als maxOccurences beinhaltet,
+        // remove es jedes Mal wenn es erneut auftaucht.
+
+        return elements
+            .filterIndexed() { index, element ->
+                elements
+                    .slice(0..index)
+                    .count{ it == element } <= maxOcurrences
+                }
+            .toIntArray()
+    }
 }
