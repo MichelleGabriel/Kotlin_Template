a7adbae7a3bb1369b868981a99972d9625c4dd9c
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-04-30 14:11:46 +0200

    Zonk first test green

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,50 @@
-class Template {
-    fun foo() = true
+fun getScore(dice: IntArray): Int {
+
+//    Combination	Example roll	Points
+//    Straight (1,2,3,4,5 and 6)	6 3 1 2 5 4	1000 points
+//    Three pairs of any dice	    2 2 4 4 1 1	750 points
+//    Three of 1	                1 4 1 1	1000 points
+//    Three of 2	                2 3 4 2 2	200 points
+//    Three of 3                	3 4 3 6 3 2	300 points
+//    Three of 4	                4 4 4	400 points
+//    Three of 5	                2 5 5 5 4	500 points
+//    Three of 6	                6 6 2 6	600 points
+//    Four of a kind	            1 1 1 1 4 6	 | 2 × Three-of-a-kind score (in example, 2000 pts)
+//    Five of a kind	            5 5 5 4 5 5	 | 3 × Three-of-a-kind score (in example, 1500 pts)
+//    Six of a kind	                4 4 4 4 4 4	 | 4 × Three-of-a-kind score (in example, 1600 pts)
+//    Every 1	                    4 3 1 2 2	100 points
+//    Every 5	                    5 2 6	50 points
+
+    var diceResult: Int = 0
+
+    return resultOf(dice)
+
+}
+
+private fun resultOf(dice: IntArray): Int {
+    if (dice.isStraight()) {
+        return 1000
+    }
+
+    if (dice.containsThreePairs()) {
+        return 750
+    }
+
+    return 0
+
+}
+
+private fun IntArray.containsThreePairs(): Boolean {
+    this.filterIndexed { index, element ->
+        this
+            .slice(1..index)
+            .count { it == element } == 3
+    }
+
+    return false
+}
+
+private fun IntArray.isStraight(): Boolean {
+    return this.toSet() == (1..6).toSet()
+
 }
--- src/test/kotlin/TemplateTest.kt
+++ src/test/kotlin/TemplateTest.kt
@@ -1,9 +1,44 @@
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-class TemplateTest {
+@DisplayName("Example tests")
+class ZonkTests {
+
     @Test
-    fun `pass`() {
-        assertTrue(true)
+    fun `straight dice roll returns 1000`() {
+        assertEquals(1000, getScore(intArrayOf(6,3,1,2,5,4)))
+        assertEquals(1000, getScore(intArrayOf(1,2,3,4,5,6)))
+    }
+
+
+    @Test
+    fun `three pairs of any dice`() {
+        assertEquals(750, getScore(intArrayOf(2,2,4,4,1,1)))
+        assertEquals(0, getScore(intArrayOf(3,2)))
+        assertEquals(1000, getScore(intArrayOf(1,2,3,4,5,6)))
+    }
+
+    @Test
+    @DisplayName("Single die")
+    fun test1() {
+        doTest(intArrayOf(1), 100)
+        doTest(intArrayOf(2), 0)
+        doTest(intArrayOf(3), 0)
+        doTest(intArrayOf(4), 0)
+        doTest(intArrayOf(5), 50)
+        doTest(intArrayOf(6), 0)
+    }
+    @Test
+    @DisplayName("Multiple dice")
+    fun test2() {
+        doTest(intArrayOf(1,1), 200)
+        doTest(intArrayOf(1,1,1), 1000)
+        doTest(intArrayOf(1,2,3,4,5,6), 1000)
+        doTest(intArrayOf(2,2,3,3,6,6), 750)
+        doTest(intArrayOf(3,2,6,4,4,6), 0)
+    }
+    private fun doTest(dice: IntArray, expected: Int) {
+        assertEquals(expected, getScore(dice), "(" + dice.joinToString() + ")")
     }
 }
