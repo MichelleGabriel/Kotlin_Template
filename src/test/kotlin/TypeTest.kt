1e6769382760068815f6531bdb4a9ef01c449847
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-05-13 16:12:52 +0200

    parameterizes PokemonType tests

--- src/test/kotlin/TypeTest.kt
+++ src/test/kotlin/TypeTest.kt
@@ -1,110 +1,67 @@
 import Type.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class TypeTest {
 
+    @ParameterizedTest(name = "is super effective when {0} attacks {1}")
+    @MethodSource("superEffectiveCases")
+    fun `handles all super effective cases`(ownType: Type, enemyType: Type) {
+        // GIVEN
+
+        // WHEN
+        val result = ownType.effectivenessMultiplier(enemyType)
+
+        // THEN
+        assertEquals(2.0, result)
+    }
+
+    @ParameterizedTest(name = "is not very effective when {0} attacks {1}")
+    @MethodSource("notVeryEffectiveCases")
+    fun `handles all not super effective cases`(ownType: Type, enemyType: Type) {
+        // GIVEN
+
+        // WHEN
+        val result = ownType.effectivenessMultiplier(enemyType)
+
+        // THEN
+        assertEquals(0.5, result)
+    }
+
+
     //Super effective
     @Test
-    fun `multiplier for - FIRE against GRASS`() {
+    fun `multiplier for neutral effectivness`() {
         //Given
 
         //When
 
         //Then
-        assertEquals(2.0, FIRE.effectivenessMultiplier(GRASS))
-    }
-
-    @Test
-    fun `multiplier for - WATER against FIRE`() {
-        //Then
-        assertEquals(2.0, WATER.effectivenessMultiplier(FIRE))
-    }
-
-    @Test
-    fun `multiplier for - GRASS against ELECTRO`() {
-        //Then
-        assertEquals(2.0, GRASS.effectivenessMultiplier(ELECTRO))
-    }
-
-    @Test
-    fun `multiplier for - ELECTRO against WATER`() {
-        //Then
-        assertEquals(2.0, ELECTRO.effectivenessMultiplier(WATER))
-    }
-
-    //Not very effective
-    @Test
-    fun `multiplier for - GRASS against FIRE`() {
-        //Then
-        assertEquals(0.5, GRASS.effectivenessMultiplier(FIRE))
-    }
-
-    @Test
-    fun `multiplier for - FIRE against WATER`() {
-        //Then
-        assertEquals(0.5, FIRE.effectivenessMultiplier(WATER))
-    }
-
-    @Test
-    fun `multiplier for - ELECTRO against GRASS`() {
-        //Then
-        assertEquals(0.5, ELECTRO.effectivenessMultiplier(GRASS))
-    }
-
-    @Test
-    fun `multiplier for - WATER against ELECTRO`() {
-        //Then
-        assertEquals(0.5, WATER.effectivenessMultiplier(ELECTRO))
-    }
-
-    //Neutral
-    @Test
-    fun `multiplier for - WATER against WATER`() {
-        //Then
-        assertEquals(1.0, WATER.effectivenessMultiplier(WATER))
-    }
-
-    @Test
-    fun `multiplier for - ELECTRO against ELECTRO`() {
-        //Then
-        assertEquals(1.0, ELECTRO.effectivenessMultiplier(ELECTRO))
-    }
-
-    @Test
-    fun `multiplier for - FIRE against FIRE`() {
-        //Then
         assertEquals(1.0, FIRE.effectivenessMultiplier(FIRE))
     }
 
-    @Test
-    fun `multiplier for - GRASS against GRASS`() {
-        //Then
-        assertEquals(1.0, GRASS.effectivenessMultiplier(GRASS))
-    }
+    companion object {
+        @JvmStatic
+        fun superEffectiveCases(): List<Arguments> {
+            return listOf(
+                Arguments.of(FIRE, GRASS),
+                Arguments.of(WATER, FIRE),
+                Arguments.of(GRASS, ELECTRO),
+                Arguments.of(ELECTRO, WATER),
+            )
+        }
 
-    @Test
-    fun `multiplier for - FIRE against ELECTRO`() {
-        //Then
-        assertEquals(1.0, FIRE.effectivenessMultiplier(ELECTRO))
+        @JvmStatic
+        fun notVeryEffectiveCases(): List<Arguments> {
+            return listOf(
+                Arguments.of(GRASS, FIRE),
+                Arguments.of(FIRE, WATER),
+                Arguments.of(ELECTRO, GRASS),
+                Arguments.of(WATER, ELECTRO),
+            )
+        }
     }
-
-    @Test
-    fun `multiplier for - ELECTRO against FIRE`() {
-        //Then
-        assertEquals(1.0, ELECTRO.effectivenessMultiplier(FIRE))
-    }
-
-    @Test
-    fun `multiplier for - WATER against GRASS`() {
-        //Then
-        assertEquals(1.0, WATER.effectivenessMultiplier(GRASS))
-    }
-
-    @Test
-    fun `multiplier for - GRASS against WATER`() {
-        //Then
-        assertEquals(1.0, GRASS.effectivenessMultiplier(WATER))
-    }
-
 }
