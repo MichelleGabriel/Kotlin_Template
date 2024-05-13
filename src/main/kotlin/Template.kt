1b5e0bf95a611c49db5ed63b508ea1f4f0e3399c
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-05-13 08:50:39 +0200

    Kata: ComplementaryDNA done

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,16 @@
-class Template {
-    fun foo() = true
+package dna
+
+fun makeComplement(dna: String): String {
+
+    return dna.map {
+        when (it) {
+            'T' -> 'A'
+            'A' -> 'T'
+            'G' -> 'C'
+            'C' -> 'G'
+            else -> {it}
+        }
+    }
+        .joinToString("")
+
 }
--- src/test/kotlin/TemplateTest.kt
+++ src/test/kotlin/TemplateTest.kt
@@ -1,9 +1,15 @@
+import dna.makeComplement
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-class TemplateTest {
+class TestExample {
     @Test
-    fun `pass`() {
-        assertTrue(true)
+    fun test01() {
+        assertEquals("TTTT", makeComplement("AAAA"));
     }
+    @Test
+    fun test02() {
+        assertEquals("TAACG", makeComplement("ATTGC"));
+    }
+
 }
