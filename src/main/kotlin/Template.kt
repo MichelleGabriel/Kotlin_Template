e5873b2548b60ad63fafaaf5d9d5c830c7eb7b04
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-05-13 09:31:11 +0200

    Kata: Even numbers in an array unfinished

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,5 @@
-class Template {
-    fun foo() = true
+fun evenNumbers(array: List<Int>, number: Int): List<Int> {
+    return array.map { if (it % 2 == 0) {
+        it
+    }}.takeLast(number)
 }
--- src/test/kotlin/TemplateTest.kt
+++ src/test/kotlin/TemplateTest.kt
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-class TemplateTest {
+class ExampleTest {
     @Test
-    fun `pass`() {
-        assertTrue(true)
+    fun basicTests() {
+        assertEquals(listOf(4, 6, 8), evenNumbers(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9), 3))
+        assertEquals(listOf(-8, 26), evenNumbers(listOf(-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26), 2))
+        assertEquals(listOf(6), evenNumbers(listOf(6, -25, 3, 7, 5, 5, 7, -3, 23), 1))
     }
 }
