12e1b64c4778de97e908c007e0d5dabcf63a7e49
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-05-15 14:58:35 +0200

    valueFor in EquipmentType ausgelagert

--- src/main/kotlin/EquipmentType.kt
+++ src/main/kotlin/EquipmentType.kt
@@ -3,6 +3,41 @@ enum class EquipmentType {
     CHEST,
     WEAPON,
     GLOVES,
-    HEAD,
+    HEAD, ;
+
+    fun giveMeValueFor(stat: Stat, defaultValue: Int): Int {
+
+        if (this == EquipmentType.WEAPON) {
+            return when (stat) {
+                Stat.ATTACK -> defaultValue
+                else -> 0
+            }
+        }
+        if (this == EquipmentType.CHEST) {
+            return when (stat) {
+                Stat.HEALTH -> defaultValue
+                else -> 0
+            }
+        }
+
+        if (this == EquipmentType.GLOVES) {
+            return when (stat) {
+                Stat.ATTACK_SPEED -> defaultValue
+                else -> 0
+            }
+        }
+
+        if (this == EquipmentType.HEAD) {
+            return when (stat) {
+                Stat.RECOVERY -> defaultValue
+                else -> 0
+            }
+        }
+
+        return 0
+    }
+
 
 }
+
+
