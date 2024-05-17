c1b3d93f0c58a82148862b3ede8f46caf44d7ed9
Michelle Gabriel <michelle.gabriel@volkswagen.de>
2024-05-17 14:25:13 +0200

    kata solved

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,3 +1,13 @@
-class Template {
-    fun foo() = true
-}
+fun points(games: List<String>) =
+    games.sumOf {
+        val (x, y) = it.split(":")
+        when {
+            x > y -> 3
+            x < y -> 0
+            else -> 1
+        }.toInt()
+    }
+
+// Own solution>
+// return games.count { it.first() > it.last() } * 3 + games.count{ it.first() == it.last()}
+// -> Edgecase, wenn Punkte mehrstellig sind funktioniert meine Loseung nicht.
